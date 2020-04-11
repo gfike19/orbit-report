d5aa96b4a4cc1b2a708f327e859b98f9c823da85
Gail Fike <gfike19@gmail.com>
2020-04-11 17:37:52 -0500

    added stripes to table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
@@ -22,12 +23,27 @@ export class AppComponent {
              let s: Satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
              this.sourceList.push(s);
           }
- 
-       }.bind(this));
-    }.bind(this));
+       // make a copy of the sourceList to be shown to the user
+       this.displayList = this.sourceList.slice(0);
+      }.bind(this));
+   }.bind(this));
+   
  
  }
 
+ search(searchTerm: string): void {
+   let matchingSatellites: Satellite[] = [];
+   searchTerm = searchTerm.toLowerCase();
+   for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
+      }
+   }
+   // assign this.displayList to be the the array of matching satellites
+   // this will cause Angular to re-make the table, but now only containing matches
+   this.displayList = matchingSatellites;
+}
  
 }
 
